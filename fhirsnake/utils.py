9a4a1bcdfd0ed1f68595a65adea0d3448fa1b869
Alex Lipovka <alex.lipovka@gmail.com>
2025-08-10 19:53:58 +0400

    Replace uri with reference in watch mode

--- fhirsnake/utils.py
+++ fhirsnake/utils.py
@@ -18,3 +18,20 @@ def substitute_env_vars(obj):
         return re.sub(r"\$\{(\w+)\}", replacer, obj)
     else:
         return obj
+
+
+def convert_uri_to_reference(uri: str) -> str:
+    if uri.startswith("urn:uuid:"):
+        split = uri.split(":")
+        return f"{split[2]}/{split[3]}"
+    return uri
+
+
+def replace_urn_uuid_with_reference(obj):
+    if isinstance(obj, str):
+        return convert_uri_to_reference(obj)
+    elif isinstance(obj, dict):
+        return {k: replace_urn_uuid_with_reference(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [replace_urn_uuid_with_reference(item) for item in obj]
+    return obj
